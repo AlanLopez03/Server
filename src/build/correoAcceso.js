ea5844db230452dd96c17295db4e3c245d752550
Ulises-Pastor <pamu010914@gs.utm.mx>
2024-04-01 01:16:38 -0600

    envio de correo

---
+++ src/build/correoAcceso.js
@@ -0,0 +1,30 @@
+"use strict";
+var email = require("emailjs/email");
+console.log("hola");
+module.exports = (formulario) => {
+    var server = email.server.connect({
+        user: "destellojoyeria3@gmail.com",
+        password: "vbuc itgi wucg cava",
+        host: "smtp.gmail.com",
+        ssl: true,
+    });
+    console.log("formulario.correo: " + formulario.correo);
+    var message = {};
+    message = {
+        from: "destellojoyeria3@gmail.com",
+        to: formulario.correo,
+        bcc: "",
+        subject: "Reestablece tu contraseña",
+        attachment: [
+            { data: `Hemos recibido una solicitud para reestablecer tu contraseña.
+            Da click en el siguiente enlace:
+
+            https://zoominformatica.com/blog/como-configurar-email-a-traves-de-una-contrasena-de-aplicacion-google/#google_vignette
+
+            
+            Si no fuiste tu, ignora este correo.`, alternative: true }
+        ]
+    };
+    server.send(message, function (err, message) { console.log(err); });
+    console.log("Llega despues.");
+};
